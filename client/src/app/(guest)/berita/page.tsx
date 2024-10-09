54ff39aa0dcfefd8b59af4b3858be4aae554f2ae
Earl <devaahmad43@gmail.com>
2024-10-09 18:49:13 +0700

    Swipe berita 1

--- client/src/app/(guest)/berita/page.tsx
+++ client/src/app/(guest)/berita/page.tsx
@@ -3,6 +3,16 @@
 import CategoryFilter from "@/components/categoryFilter/CategoryFilter"; // Sesuaikan dengan path ke CategoryFilter
 import BeritaCard from "@/components/beritaCard/BeritaCard"; // Sesuaikan dengan path ke BeritaCard
 import axios from "axios";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 
 export default async function Home() {
   const { data } = await axios.get('http://127.0.0.1:8000/api/berita/get')
@@ -17,33 +27,48 @@ export default async function Home() {
       <CategoryFilter />
 
       {/* Berita dengan Navigasi */}
-      <div className="relative flex items-center mt-8">
+      {/* <div className="relative flex items-center mt-8"> */}
         {/* Left Navigation Button */}
-        <button className=" bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-full shadow-lg transition-all duration-300 z-10">
+        {/* <button className=" bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-full shadow-lg transition-all duration-300 z-10">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
           </svg>
-        </button>
+        </button> */}
         {/* Berita card nya di loop */}
         {/* Berita Cards */}
-        <div className="flex overflow-x-scroll space-x-6 pl-12 pr-12 py-4 scrollbar-hide">
-          {data.data.map((dataItem) => (
-            <BeritaCard
-              imageSrc={dataItem.foto}
-              category={dataItem.kategori}
-              title={dataItem.judul}
-            />
+        <div className="flex overflow-x-scroll space-x-6 pl-12 pr-12 py-4 scrollbar-hide h">
+            <Swiper
+            
+            slidesPerView={3}
+            centeredSlides={true}
+            spaceBetween={30}
+            pagination={{
+              type: 'fraction',
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            {data.data.map((dataItem) => (
+            <SwiperSlide key={dataItem.id_berita}>
+              <BeritaCard
+                imageSrc={dataItem.foto}
+                category={dataItem.kategori}
+                title={dataItem.judul}
+              />
+            </SwiperSlide>
           ))}
+          </Swiper>
           {/* Tambahkan lebih banyak BeritaCard jika diperlukan */}
         </div>
 
         {/* Right Navigation Button */}
-        <button className=" bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-full shadow-lg transition-all duration-300 z-10">
+        {/* <button className=" bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-full shadow-lg transition-all duration-300 z-10">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
           </svg>
-        </button>
-      </div>
+        </button> */}
+      {/* </div> */}
     </div>
   );
 }
