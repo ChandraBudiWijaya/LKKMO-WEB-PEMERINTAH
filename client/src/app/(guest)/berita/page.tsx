d9afb36bf015c6aad2206d0cada95a173d94d0b0
ChandraBudiWijaya <chandrabw.cjcc@gmail.com>
2024-10-11 07:51:58 +0700

    foldering public, adjusment page berita, and change path photo

--- client/src/app/(guest)/berita/page.tsx
+++ client/src/app/(guest)/berita/page.tsx
@@ -23,9 +23,6 @@ export default async function Home() {
       {/* Header Berita */}
       <h1 className="text-3xl font-bold text-center mb-6">BERITA DAERAH</h1>
 
-      {/* Category Filter */}
-      <CategoryFilter />
-
       {/* Berita dengan Navigasi */}
       <div className="relative flex items-center mt-8">
         {/* Left Navigation Button */}
@@ -73,13 +70,6 @@ export default async function Home() {
           </Swiper>
           {/* Tambahkan lebih banyak BeritaCard jika diperlukan */}
         </div>
-
-        {/* Right Navigation Button */}
-        {/* <button className=" bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-full shadow-lg transition-all duration-300 z-10">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
-          </svg>
-        </button> */}
       </div>
     </div>
   );
